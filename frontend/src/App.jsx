78ba1529ca2f80141eea92c4911cd61a83257fdd
Moiz <moizayub401@gmail.com>
2025-09-27 19:17:11 +0500

    home page complete done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
+import { Toaster } from "sonner";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Toaster position="top-right" />
       <Routes>
         {/* User Layout */}
         <Route path="/" element={<UserLayout />}>
-        <Route index element={<Home/>}/>
+          <Route index element={<Home />} />
         </Route>
 
         <Route>{/* Admin Layout */}</Route>
