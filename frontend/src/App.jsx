be82c3beb3423641b845bd77f124a0b538f672dd
Moiz <moizayub401@gmail.com>
2025-10-04 06:37:28 +0500

    worked on AdminLayout, AdminSideBar , AdminHomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ import Checkout from "./components/Cart/Checkout";
 import OrderConfirmationPage from "./pages/OrderConfirmationPage";
 import OrderDetailsPage from "./pages/OrderDetailsPage";
 import MyOrderPage from "./pages/MyOrderPage";
+import AdminLayout from "./components/Admin/AdminLayout";
+import AdminHomePage from "./pages/AdminHomePage";
 
 const App = () => {
   return (
@@ -35,7 +37,10 @@ const App = () => {
           <Route path="my-orders" element={<MyOrderPage />} />
         </Route>
 
-        <Route>{/* Admin Layout */}</Route>
+        {/* Admin Layout */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminHomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
