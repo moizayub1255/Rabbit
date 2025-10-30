4f84faf8a2a6cc71d13f2522fd04617fb881c780
Moiz <moizayub401@gmail.com>
2025-10-31 00:04:44 +0500

    day 2 of backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
@@ -44,6 +45,54 @@ router.post("/register", async (req, res) => {
     console.log(error);
     res.status(500).send("Server Error");
   }
+});
+
+// @route POST /api/users/login
+// @desc Authenticate user
+// @access Public
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // Find user by email
+    let user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    const payload = { id: user._id, role: user.role };
+
+    //sign and return the token along with the user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "3d" },
+      (err, token) => {
+        if (err) throw err;
+        res.json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route GET /api/users/profile
+// @desc Get logged-in user's profile (Protected Route)
+// @access Private
+router.get("/profile", protect, async (req, res) => {
+  res.json(req.user);
 });
 
 module.exports = router;
